67854f6e0cfccbfc46d3f51f21602fc3bd33bbaa
Matéo Dominguez <mateleo78@gmail.com>
2021-03-11 09:37:06 +0100

    remove globals variables Everthing is on function

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-fr = open("dicofr.txt","r",encoding="utf-8")
-letterList = ['a','e','i','u','s','o','v','t','n','\n']
-
 #function to test if a word is using only those letters
 # return boolean
 def WordTester(myword):
+    letterList = ['a','e','i','u','s','o','v','t','n','\n']
     for letter in myword:
         if not letter in letterList:
             return False
@@ -19,19 +17,20 @@ def ForkGrabber(x,y,word):
         return True
     else:
         return False
-        
+
 #export to a file. couter is to know the size
 def export():
-    counter = 0
+    fr = open("dicofr.txt","r",encoding="utf-8")
     exp = open('Output/export.txt',"w+",encoding="utf-8")
+    counter = 0
     for word in fr:
         word = WordCleaner(word)
         if(WordTester(word) and ForkGrabber(5, 7, word) and counter<60):
             exp.write(word+" ")
             counter+=1
     exp.close()
+    fr.close()
 
 #main
 
 export()
-fr.close()
